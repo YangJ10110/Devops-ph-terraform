20c9e0b0e76e10c1dfff3adc59c892fea8d7e725
YangJ10110 <jeromeyang21@gmail.com>
2025-01-19 03:42:45 +0800

    main

---
+++ localstack/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    skip_requesting_account_id = true
+    
+    endpoints {
+      ec2 = "http://localhost:4566"
+
+    }
+}
+
+resource "aws_instance" "web-server" {
+    ami = "ami-06ca3ca175f37dd66"
+    instance_type = "t2.micro"
+    count = 5
+    tags = {
+      Name = "web-server-${count.index}"
+    }
+}
+
